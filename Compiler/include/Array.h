91463c293ee0f7d0185c4983224020aab85b3495
Mel Florance <mel.florance@gmail.com>
2020-07-26 03:20:44 +0200

    Improve arrays functionnalities.

--- Compiler/include/Array.h
+++ Compiler/include/Array.h
@@ -6,5 +6,9 @@ class Array : public Type {
 public:
 	Array(const std::variant<double, int, bool, Function*, std::string, std::vector<Type*>>& value = 0);
 
+	std::pair<Type*, Error*> compare_less_than(Type* other) override;
+	std::pair<Type*, Error*> subtract(Type* other) override;
 	std::pair<Type*, Error*> add(Type* other) override;
+	std::pair<Type*, Error*> compare_greater_than(Type* other) override;
+	std::pair<Type*, Error*> modulus(Type* other) override;
 };
--- Compiler/src/Array.cpp
+++ Compiler/src/Array.cpp
@@ -8,11 +8,63 @@ Array::Array(const std::variant<double, int, bool, Function*, std::string, std::
 {
 }
 
-std::pair<Type*, Error*> Array::add(Type* other)
+std::pair<Type*, Error*> Array::compare_less_than(Type* other)
 {
 	auto array = std::get<std::vector<Type*>>(value);
 	array.push_back(other);
 	value = std::move(array);
 
 	return std::make_pair(this, nullptr);
+}
+
+std::pair<Type*, Error*> Array::subtract(Type* other)
+{
+	auto array = std::get<std::vector<Type*>>(value);
+
+	if (other->value.index() == 1 && array.size() > 0) {
+		auto index = std::get<int>(other->value);
+
+		if (index < array.size() && index >= 0) {
+			array.erase(array.begin() + index);
+			value = std::move(array);
+		}
+	}
+
+	return std::make_pair(this, nullptr);
+}
+
+std::pair<Type*, Error*> Array::add(Type* other)
+{
+	auto array = std::get<std::vector<Type*>>(value);
+	auto elements = std::get<std::vector<Type*>>(other->value);
+	std::vector<Type*>::iterator it = elements.begin();
+
+	for (; it != elements.end(); ++it)
+		array.push_back(*it);
+	
+	value = std::move(array);
+
+	return std::make_pair(this, nullptr);
+}
+
+std::pair<Type*, Error*> Array::compare_greater_than(Type* other)
+{
+	auto array = std::get<std::vector<Type*>>(value);
+
+	if (other->value.index() == 1) {
+		auto index = std::get<int>(other->value);
+
+		if (index < array.size()) {
+			auto element = array.at(index);
+			return std::make_pair(element, nullptr);
+		}
+	}
+
+	return std::pair<Type*, Error*>();
+}
+
+std::pair<Type*, Error*> Array::modulus(Type* other)
+{
+	auto array = std::get<std::vector<Type*>>(value);
+	return std::make_pair(new Number((int)array.size()), nullptr);
 }
